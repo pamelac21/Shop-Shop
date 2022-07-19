2d0a9d7ab75a8711f10d3cec13fb107d05070c00
PamelaCleveland <pamelac021@gmail.com>
2022-07-19 14:25:09 -0400

    Create Layout for Shopping Cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import Cart from '../components/Cart';
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from '../utils/queries';
@@ -51,6 +52,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
